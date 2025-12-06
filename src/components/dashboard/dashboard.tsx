9df49ff296439f56f519a86b61e8e408c11f0ce4
Aravind S <sivam3882@gmail.com>
2025-12-06 05:09:23 +0000

    and i want all the options in top like a  rows so that will good i think

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -8,17 +8,13 @@ import { WeatherReschedule } from "./weather-reschedule";
 
 export function Dashboard() {
   return (
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-      <div className="space-y-8 lg:col-span-2">
+    <div className="flex flex-col gap-8">
         <BookingCalendar />
         <AddonsBooking />
-        <WeatherReschedule />
-      </div>
-      <div className="space-y-8 lg:col-span-1">
         <VenueInfo />
         <LoyaltyStatus />
         <RecentBookings />
-      </div>
+        <WeatherReschedule />
     </div>
   );
 }
